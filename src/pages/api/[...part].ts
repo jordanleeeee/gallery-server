7a86889b55972685b6f44ecf876c90b05777e904
JordanLee <jordanlee80@gmail.com>
2023-03-18 19:39:01 +0800

    add image compress

--- src/pages/api/[...part].ts
+++ src/pages/api/[...part].ts
@@ -1,14 +1,20 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
+import imagemin from 'imagemin'
+import imageminMozjpeg from 'imagemin-mozjpeg'
 import * as fs from "fs";
 import {getContentType, getRootPath} from "@/util/fileUtil";
 import {decode} from "@/util/urlUtil";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     let path = getRootPath() + decode(req.url!.substring(4, req.url?.length))
+    const fromLocal = req.socket.remoteAddress === '::1' || req.socket.remoteAddress === '::ffff:127.0.0.1'
 
     try {
         let buffer = fs.readFileSync(path);
         res.writeHead(200, {'Content-Type': getContentType(path)});
+        if (!fromLocal && buffer.length > 100_000) { // compress for non-local request and file > 1MB
+            buffer = await imagemin.buffer(buffer, {plugins: [imageminMozjpeg({quality: 80})]});
+        }
         res.end(buffer)
     } catch (e) {
         res.status(404);
